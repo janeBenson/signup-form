1b18ec428f83fdfe5c3da167b21ee86bfc78ea3c
Jane Benson <jane.benson1994@gmail.com>
2024-04-28 16:48:42 -0700

    Refactor password validation code

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 const helpText = document.querySelector('#pw-help-text')
 const form = document.querySelector('form')
 
+function displayEl(el) {
+	el.style.display = 'block'
+}
+function hideEl(el) {
+	el.style.display = 'none'
+}
+function addClass(el, className) {
+	el.classList.add(className)
+}
+function removeClass(el, className) {
+	el.classList.remove(className)
+}
+
 function validatePasswords() {
-	const pw1 = document.querySelector('#password').value
-	const pw2 = document.querySelector('#confirm-password').value
-	if (!pw1 && !pw2) {
+	const pw1 = document.querySelector('#password')
+	const pw2 = document.querySelector('#confirm-password')
+	if (!pw1.value && !pw2.value) {
 		return false
 	}
-	if (pw1 !== pw2) {
-		helpText.style.display = 'block'
+	if (pw1.value !== pw2.value) {
+		displayEl(helpText)
+		addClass(pw1, 'invalid')
+		addClass(pw2, 'invalid')
 		return false
 	} else {
-		helpText.style.display = 'none'
+		hideEl(helpText)
+		removeClass(pw1, 'invalid')
+		removeClass(pw2, 'invalid')
+
 		return true
 	}
 }
